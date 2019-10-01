dc6311fbddd87a1331d44d64d540389278422c2d
Anatoly <tolik_83@mail.ru>
2019-10-01 23:41:33 +0300

    v 1.0.0

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="Menu">
+      
+      </div>
+      <div className="mainArea">
+        <div className="header">
+
+        </div>
+        <div className="content">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
