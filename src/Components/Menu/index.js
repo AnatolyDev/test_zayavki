8e2fd1b246aa0d524bc717c449fa9bb0c327712c
Anatoly <tolik_83@mail.ru>
2019-10-03 16:46:06 +0300

    меню навигации v. 2

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -13,79 +13,78 @@ import imgSettings from '../../Icons/Settings.png';
 const Menu = () => {
     return (
         <>
-            <Nav vertical>
-                <NavItem>
-                    <NavLink to="#">
+            <Nav vertical>                
+                
+                    <NavItem>
                         <img src={imgLogo} alt=''/>
-                    </NavLink>
-                </NavItem>
-
-                <NavItem>
-                    <NavLink to="/base">
+                    </NavItem>
+                
+            
+                <NavLink to="/base">
+                    <NavItem>
                         <div>
                             <img src={imgBase} alt=''/>
                         </div>
                         <div>
                             База знаний
                         </div>
-                    </NavLink>
-                </NavItem>
-
-                <NavItem>
-                    <NavLink to="/zayavki">
+                    </NavItem>
+                </NavLink>
+                
+                <NavLink to="/zayavki">
+                    <NavItem>
                         <div>
                             <img src={imgZayavki} alt=''/>
                         </div>
                         <div>
                             Заявки
                         </div>
-                    </NavLink>
-                </NavItem>
-
-                <NavItem>
-                    <NavLink to="/people">
+                    </NavItem>
+                </NavLink>
+                
+                <NavLink to="/people">
+                    <NavItem>
                         <div>
                             <img src={imgPeople} alt=''/>
                         </div>
                         <div>
                             Сотрудники
                         </div>
-                    </NavLink>
-                </NavItem>
-
-                <NavItem>
-                    <NavLink to="/clients">
+                    </NavItem>
+                </NavLink>
+                
+                <NavLink to="/clients">
+                    <NavItem>
                         <div>
                             <img src={imgClients} alt=''/>
                         </div>
                         <div>
                             Клиенты
                         </div>
-                    </NavLink>
-                </NavItem>
-
-                <NavItem>
-                    <NavLink to="/assets">
+                    </NavItem>
+                </NavLink>
+                
+                <NavLink to="/assets">
+                    <NavItem>
                         <div>
                             <img src={imgAssets} alt=''/>
                         </div>
                         <div>
                             Активы
                         </div>
-                    </NavLink>
-                </NavItem>
-
-                <NavItem>
-                    <NavLink to="/settings">
+                    </NavItem>
+                </NavLink>
+                
+                <NavLink to="/settings">
+                    <NavItem>
                         <div>
                             <img src={imgSettings} alt=''/>
                         </div>
                         <div>
                             Настройки
                         </div>
-                    </NavLink>
-                </NavItem>
-
+                    </NavItem>
+                </NavLink>
             </Nav>
         </>
     )
